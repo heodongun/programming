38bd68bb28bd7e78b8bb0a14cbda2edc03288daa
heodongun <heodongun0922@gmail.com>
2024-09-05 15:41:32 +0900

    pyrhon

---
+++ Main.py
@@ -0,0 +1,82 @@
+#print("Hello")
+
+#print("Hello world")
+
+# print("Hello")
+
+# print("world")
+
+# print("'Hello'")
+
+# a="C:\Download\\'hello'.py"
+
+# print('"'+a+'"')
+
+#print('print("Hello\\nWorld")')
+
+# a=input()
+
+# for i in range(3):
+
+#     print(a)
+
+# a=input().split()
+
+# print(a[1])
+
+# print(a[0])
+
+# a=input().split()
+# print(a[1])
+# print(a[0])
+
+# a=input()
+# for i in range(3):
+#     print(a)
+
+# a=input()
+# print(a)
+
+# a=input()
+# for i in a:
+#     print(i)
+
+# a=input().split()
+# print(a[0]+a[1])
+
+# a=input()
+# b=input()
+# b=float(a)+float(b)
+# print(b)
+
+# a=int(input(),16)
+# print('%o'%a)
+
+# n = ord(input())
+# print(n)
+
+# c = int(input())
+# print(chr(c))
+
+# n=int(input())
+# print(-n)
+
+# n=ord(input())
+# print(chr(n+1))
+
+# one=int(input().split())
+# print(one[0]-one[1])
+
+# a = list(map(int, input().split()))
+#
+# print(a[0]-a[1])
+
+# a=input().split()
+# print(int(a[1])*a[0])
+
+# a=int(input())
+# b=input()
+# print(a*b)
+
+a=input().split()
+print(int(a[0])**int(a[1]))
