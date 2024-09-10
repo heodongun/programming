a976f02142acc0f7e34ca40054bff6f2eef0598b
heodongun <heodongun0922@gmail.com>
2024-09-10 23:45:45 +0900

    first

--- Main.py
+++ Main.py
@@ -121,8 +121,89 @@
 # else:
 #     print("True")
 
-a=input().split()
-if int(a[0])!=0 and int(a[1])!=0:
-    print("True")
-else:
-    print("False")
+# a=input().split()
+# if int(a[0])!=0 and int(a[1])!=0:
+#     print("True")
+# else:
+#     print("False")
+#
+# a=input().split()
+# if int(a[0])|int(a[1]):
+#     print("True")
+# else:
+#     print("False")
+
+# # 두 개의 정수를 입력받기
+# a, b = input().split()
+#
+# # 정수를 bool 타입으로 변환
+# c = bool(int(a))
+# d = bool(int(b))
+#
+# # XOR 연산을 통해 서로 다른 경우에만 True 출력
+# print((c and not d) or (not c and d))
+
+# a=int(input())
+# print(~a)
+
+# a=input().split()
+# print(int(a[0])&int(a[1]))
+
+# a=input().split()
+# print(int(a[0])|int(a[1]))
+
+# a=input().split()
+# print(int(a[0])^int(a[1]))
+
+# a=input().split()
+# if int(a[0])>int(a[1]):
+#     print(a[0])
+# else:
+#     print(a[1])
+
+# a=int(input())
+# if a<0:
+#     if a%2==0:
+#         print('A')
+#     else:
+#         print('B')
+# else:
+#     if a%2==0:
+#         print('C')
+#     else:
+#         print('D')
+
+# score = int(input())
+# grade = 'winter' if  score==12 or score==1 or score==2 else (
+#         'spring' if  score==3 or score==4 or score==5 else (
+#         'summer' if  score==6 or score==7 or score==8 else (
+#         'fall' if score==9 or score==10 or score==11 else())))
+#
+# print(grade)
+
+# a=int(input())
+# while(a!=0):
+#         print(a)
+#         a=int(input())
+
+# a=int(input())
+# for i in range(a):
+#         print(a)
+#         a-=1
+
+# a=int(input())
+# for i in range(a):
+#         print(a-1)
+#         a-=1
+
+# a=ord('a')
+# b=ord(input())
+# for i in range(a,b+1):
+#         print(chr(i),end=' ')
+
+a=int(input())
+sum=0
+for i in range(1,a+1):
+        if i%2==0:
+                sum=sum+i
+print(sum)
